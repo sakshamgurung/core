5681f1afe11ee610e998face679f917bc4f31f10
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2021-03-25 12:40:48 +0100

    Enable streaming for propfind

--- apps/dav/lib/Server.php
+++ apps/dav/lib/Server.php
@@ -222,6 +222,7 @@ class Server {
 		));
 
 		if ($this->isRequestForSubtree(['files', 'trash-bin', 'public-files'])) {
+			\Sabre\DAV\Server::$streamMultiStatus = true;
 			$this->server->addPlugin(new ViewOnlyPlugin(
 				OC::$server->getLogger()
 			));
